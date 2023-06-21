4f1412f9c5cb4b77b025ff550874b85424467829
kmarellano <arellanokurt0130@gmail.com>
2023-06-21 16:17:43 +0800

    fix: deploy connection strings

--- startup/db.js
+++ startup/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
 	try {
-		await mongoose.connect(process.env.MONGODB_URI + process.env.DB_COLL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+		await mongoose.connect(
+			`mongodb+srv://kurt:${process.env.DB_PASSWORD}@cluster0.7gdpzrx.mongodb.net/${process.env.DB_COLL}`,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
+		);
 		console.log("connected to db");
 	} catch (err) {
 		throw err;
